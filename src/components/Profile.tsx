0e2af997de8f6b733ef0328e1fb597e1deab0546
Lucas Assis <lucas.assis_@hotmail.com>
2021-02-25 09:40:09 -0400

    fourth day

--- src/components/Profile.tsx
+++ src/components/Profile.tsx
@@ -1,14 +1,18 @@
+import { useContext } from 'react'
+import { ChallengesContext } from '../contexts/ChallengesContext'
 import styles from '../styles/components/Profile.module.css'
 
 export function Profile() {
+    const { level } = useContext(ChallengesContext);
+    
     return (
         <div className={styles.profileContainer}>
             <img src="https://github.com/lucasassis0.png" alt="Lucas Assis" />
             <div>
                 <strong>Lucas Assis</strong>
                 <p>
-                    <img src="icons/level.svg" alt="Level"/>
-                    Level 1
+                    <img src="icons/level.svg" alt="Level" />
+                    Level {level}
                 </p>
             </div>
         </div>
